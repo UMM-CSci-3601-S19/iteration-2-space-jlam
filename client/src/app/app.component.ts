8515d54dbfa1f1f88435502a3b4d02fa204252f4
smit9025 <Z321vmxw>
2019-04-04 15:14:11 -0500

    Implementing login so much more than anything at all

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,110 +1,122 @@
-import {Component, OnInit} from '@angular/core';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {environment} from '../environments/environment';
-import {AppService} from "./app.service";
+// import {Component, OnInit} from '@angular/core';
+// import {HttpClient, HttpHeaders} from '@angular/common/http';
+// import {environment} from '../environments/environment';
+// import {AppService} from "./app.service";
+//
+// declare let gapi: any;
+//
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css'],
+//   providers: [AppService]
+// })
+// export class AppComponent implements OnInit {
+//
+//   googleAuth;
+//   userFullName: string;
+//   userFirstName: string;
+//   userEmail: string;
+//
+//
+//   constructor(private http: HttpClient, public appService: AppService,) {
+//
+//   }
+//
+//
+//   title = 'Mo-Ride';
+//
+//   // This signs in the user and opens the window for signing in
+//   signIn() {
+//     this.googleAuth = gapi.auth2.getAuthInstance();
+//     console.log(" This is google Auth " + this.googleAuth);
+//     this.googleAuth.grantOfflineAccess().then((resp) => {
+//       localStorage.setItem('isSignedIn', 'true');
+//       this.sendAuthCode(resp.code);
+//     });
+//   }
+//
+//   signOut() {
+//     this.handleClientLoad();
+//
+//     this.googleAuth = gapi.auth2.getAuthInstance();
+//
+//     this.googleAuth.then(() => {
+//       this.googleAuth.signOut();
+//       localStorage.setItem('isSignedIn', 'false');
+//       localStorage.setItem("userID", "");
+//       window.location.reload();
+//     })
+//   }
+//
+//   // This sends the auth code of our user to the server and stores the fields in local storage when we get data back
+//   // from gapi
+//   sendAuthCode(code: string): void {
+//     const httpOptions = {
+//       headers: new HttpHeaders({
+//         'Content-Type': 'application/json'
+//       }),
+//     };
+//
+//     this.http.post(environment.API_URL + "login", {code: code}, httpOptions)
+//       .subscribe(onSuccess => {
+//         console.log("Code sent to server");
+//         console.log(onSuccess["_id"]);
+//         console.log(onSuccess["_id"]["$oid"]);
+//         console.log(onSuccess["email"]);
+//         console.log(onSuccess["fullName"]);
+//         console.log(onSuccess["lastName"]);
+//         console.log(onSuccess["firstName"]);
+//         localStorage.setItem("_id", onSuccess["_id"]);
+//         localStorage.setItem("oid", onSuccess["_id"]["$oid"]);
+//         localStorage.setItem("email", onSuccess["email"]);
+//         localStorage.setItem("userFullName", onSuccess["fullName"]);
+//         localStorage.setItem("userLastName", onSuccess["lastName"]);
+//         localStorage.setItem("userFirstName", onSuccess["firstName"]);
+//
+//       }, onFail => {
+//         console.log("ERROR: Code couldn't be sent to the server");
+//       });
+//   }
+//
+//
+//   getUsername () {
+//     this.userFullName = localStorage.getItem("userFullName");
+//     /*if (this.userFullName.length > 18) {
+//       this.userFullName = this.userFullName.slice(0, 17) + "...";
+//     }*/
+//     return this.userFullName;
+//   }
+//
+//
+//   handleClientLoad() {
+//     gapi.load('client:auth2', this.initClient);
+//   }
+//
+//   initClient() {
+//     gapi.client.init({
+//       'clientId': '207007000749-dgpb2fb5f4grk2r7ep34j9qhgl8jvvu3.apps.googleusercontent.com',
+//       'scope': 'profile email'
+//     });
+//   }
+//
+//
+//   ngOnInit() {
+//     this.handleClientLoad();
+//     this.getUsername();
+//     /*gapi.load('client:auth2', this.initClient);*/
+//   }
+//
+// }
+//
 
-declare let gapi: any;
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [AppService]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-
-  googleAuth;
-  userFullName: string;
-  userFirstName: string;
-  userEmail: string;
-
-
-  constructor(private http: HttpClient, public appService: AppService,) {
-
-  }
-
-
-  title = 'Mo-Ride';
-
-  // This signs in the user and opens the window for signing in
-  signIn() {
-    this.googleAuth = gapi.auth2.getAuthInstance();
-    console.log(" This is google Auth " + this.googleAuth);
-    this.googleAuth.grantOfflineAccess().then((resp) => {
-      localStorage.setItem('isSignedIn', 'true');
-      this.sendAuthCode(resp.code);
-    });
-  }
-
-  signOut() {
-    this.handleClientLoad();
-
-    this.googleAuth = gapi.auth2.getAuthInstance();
-
-    this.googleAuth.then(() => {
-      this.googleAuth.signOut();
-      localStorage.setItem('isSignedIn', 'false');
-      localStorage.setItem("userID", "");
-      window.location.reload();
-    })
-  }
-
-  // This sends the auth code of our user to the server and stores the fields in local storage when we get data back
-  // from gapi
-  sendAuthCode(code: string): void {
-    const httpOptions = {
-      headers: new HttpHeaders({
-        'Content-Type': 'application/json'
-      }),
-    };
-
-    this.http.post(environment.API_URL + "login", {code: code}, httpOptions)
-      .subscribe(onSuccess => {
-        console.log("Code sent to server");
-        console.log(onSuccess["_id"]);
-        console.log(onSuccess["_id"]["$oid"]);
-        console.log(onSuccess["email"]);
-        console.log(onSuccess["fullName"]);
-        console.log(onSuccess["lastName"]);
-        console.log(onSuccess["firstName"]);
-        localStorage.setItem("_id", onSuccess["_id"]);
-        localStorage.setItem("oid", onSuccess["_id"]["$oid"]);
-        localStorage.setItem("email", onSuccess["email"]);
-        localStorage.setItem("userFullName", onSuccess["fullName"]);
-        localStorage.setItem("userLastName", onSuccess["lastName"]);
-        localStorage.setItem("userFirstName", onSuccess["firstName"]);
-
-      }, onFail => {
-        console.log("ERROR: Code couldn't be sent to the server");
-      });
-  }
-
-
-  getUsername () {
-    this.userFullName = localStorage.getItem("userFullName");
-    /*if (this.userFullName.length > 18) {
-      this.userFullName = this.userFullName.slice(0, 17) + "...";
-    }*/
-    return this.userFullName;
-  }
-
-
-  handleClientLoad() {
-    gapi.load('client:auth2', this.initClient);
-  }
-
-  initClient() {
-    gapi.client.init({
-      'clientId': '207007000749-dgpb2fb5f4grk2r7ep34j9qhgl8jvvu3.apps.googleusercontent.com',
-      'scope': 'profile email'
-    });
-  }
-
-
-  ngOnInit() {
-    this.handleClientLoad();
-    this.getUsername();
-    /*gapi.load('client:auth2', this.initClient);*/
-  }
-
+export class AppComponent {
+  title = 'Mongo-Angular-Spark lab';
 }
